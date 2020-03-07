481b5c2fee1535107a22fb40c238eca87b2a60fc
kamil-janas <janko6684@gmail.com>
2020-03-07 11:06:48 +0100

    first file

---
+++ Pai/1_struktura.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+
+    <head>
+    <meta charset="utf-8">
+    <title>Struktura dokumentu</title>
+    </head>
+
+
+    <body>
+
+        <?php
+        echo "Hello world";
+        ?>
+
+    </body>
+
+</html>
+
